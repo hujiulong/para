ff418cbe174da5bb1e94d5f0bfc8b7850d9e0f3d
hujiulong <me@hujiulong.com>
2017-03-14 21:00:28 +0800

    clean up

--- Para.js
+++ Para.js
@@ -1,323 +1,316 @@
-var Para = Para || function( numOrArray ) {
+var Para = Para || function ( numOrArray ) {
 
-	"use strict";
+    "use strict";
 
-	this.array = null;
+    this.array = null;
 
-	if ( numOrArray instanceof Array ) {
+    var _length = 0;
+    var _size = 0;
 
-		this.array = new Float32Array( numOrArray.length * 4 );
+    if ( Array.isArray( numOrArray ) ) {
 
-		this.array.set( numOrArray );
+        _length = numOrArray.length;
+        _size = Math.ceil( Math.sqrt( _length / 4 ) );
 
-		console.log( this.array );
+        this.array = new Float32Array( _size * _size * 4 );
+        this.array.set( numOrArray );
 
+    } else if ( typeof ( numOrArray ) === 'number' ) {
 
-	} else if ( typeof( numOrArray ) === 'number' ) {
+        _length = numOrArray;
+        _size = Math.ceil( Math.sqrt( _length / 4 ) );
 
-		this.array = new Float32Array( numOrArray * 4 );
+        this.array = new Float32Array( _size * _size * 4 );
 
-	} else {
+    } else {
 
-		// error
+        throw new Error( 'parm error' );
 
-	}
+    }
 
-	var gl = getGLContext( document.createElement( 'canvas' ) );
+    var gl = getGLContext( document.createElement( 'canvas' ) );
 
-	var size = Math.sqrt( this.array.length ) / 4;
-	
-	/*
-	  a * * * * * d
-		* *     *
-		*   *   *
-		*     * *
-	  b * * * * * c
-	 */
-	var position 	= new Float32Array( [ -1, -1, 1, -1, -1, 1, 1, 1 ] );
-	var uv 			= new Float32Array( [  0,  0, 1,  0,  0, 1, 1, 1 ] );
-	var index 		= new Uint16Array( [ 0, 1, 2, 2, 1, 3 ] );		// bca acd
+    /*
+      a * * * * * d
+    	* *     *
+    	*   *   *
+    	*     * *
+      b * * * * * c
+     */
+    var position = new Float32Array( [ -1, -1, 1, -1, -1, 1, 1, 1 ] );
+    var uv = new Float32Array( [ 0, 0, 1, 0, 0, 1, 1, 1 ] );
+    var index = new Uint16Array( [ 0, 1, 2, 2, 1, 3 ] ); // bca acd
 
-	var positionBuffer = gl.createBuffer();
-	gl.bindBuffer( gl.ARRAY_BUFFER, positionBuffer );
-	gl.bufferData( gl.ARRAY_BUFFER, position, gl.STATIC_DRAW );
+    var positionBuffer = gl.createBuffer();
+    gl.bindBuffer( gl.ARRAY_BUFFER, positionBuffer );
+    gl.bufferData( gl.ARRAY_BUFFER, position, gl.STATIC_DRAW );
 
-	var uvBuffer = gl.createBuffer();
-	gl.bindBuffer( gl.ARRAY_BUFFER, uvBuffer );
-	gl.bufferData( gl.ARRAY_BUFFER, uv, gl.STATIC_DRAW );
+    var uvBuffer = gl.createBuffer();
+    gl.bindBuffer( gl.ARRAY_BUFFER, uvBuffer );
+    gl.bufferData( gl.ARRAY_BUFFER, uv, gl.STATIC_DRAW );
 
-	var indexBuffer = gl.createBuffer();
-	gl.bindBuffer( gl.ELEMENT_ARRAY_BUFFER, indexBuffer );
-	gl.bufferData( gl.ELEMENT_ARRAY_BUFFER, index, gl.STATIC_DRAW );
+    var indexBuffer = gl.createBuffer();
+    gl.bindBuffer( gl.ELEMENT_ARRAY_BUFFER, indexBuffer );
+    gl.bufferData( gl.ELEMENT_ARRAY_BUFFER, index, gl.STATIC_DRAW );
 
-	var dataTexture = createDataTexture( this.array );
-	var resultTexture = createDataTexture( new Float32Array( this.array.length ) );
+    var dataTexture = createDataTexture( this.array );
+    var resultTexture = createDataTexture( new Float32Array( this.array.length ) );
 
-	// gl.viewport( 0, 0, size, size );
-	// gl.bindFramebuffer( gl.FRAMEBUFFER, gl.createFramebuffer() );
-	// gl.framebufferTexture2D( gl.FRAMEBUFFER, gl.COLOR_ATTACHMENT0, gl.TEXTURE_2D, resultTexture, 0 );
+    // gl.viewport( 0, 0, size, size );
+    // gl.bindFramebuffer( gl.FRAMEBUFFER, gl.createFramebuffer() );
+    // gl.framebufferTexture2D( gl.FRAMEBUFFER, gl.COLOR_ATTACHMENT0, gl.TEXTURE_2D, resultTexture, 0 );
 
-	var vertexShaderCode = 
-		'attribute vec2 a_position;\n' +
-		'attribute vec2 a_uv;\n' +
-		'varying vec2 v_uv;\n' +
-		'void main( void ) {\n' +
-		'	v_uv = a_uv;\n' +
-		'	gl_Position = vec4( a_position.xy, 0.0, 1.0 );\n' +
-		"}\n";
 
-	var fragmentShaderCodeHead = 
-		'precision mediump float;\n' +
-		'uniform sampler2D u_texture;\n' +
-		'varying vec2 v_uv;\n' +
-		'void main( void ) {\n' +
-		'	vec4 value = texture2D( u_texture, v_uv );\n';
+    this.vsCode =
+        'attribute vec2 a_position;\n' +
+        'attribute vec2 a_uv;\n' +
+        'varying vec2 v_uv;\n' +
+        'void main( void ) {\n' +
+        '	v_uv = a_uv;\n' +
+        '	gl_Position = vec4( a_position.xy, 0.0, 1.0 );\n' +
+        "}\n";
 
-	var fragmentShaderCodeTail =
-		'	gl_FragColor = value;\n' +
-		'}\n';
+    var fragmentShaderCodeHead =
+        'precision mediump float;\n' +
+        'uniform sampler2D u_texture;\n' +
+        'varying vec2 v_uv;\n' +
+        'void main( void ) {\n' +
+        '	vec4 value = texture2D( u_texture, v_uv );\n';
 
+    var fragmentShaderCodeTail =
+        '	gl_FragColor = value;\n' +
+        '}\n';
 
-	this._fragmentShaderCode = fragmentShaderCodeHead;
 
+    this.fsCode = fragmentShaderCodeHead;
 
-	this.refreshData = function() {
 
-		var program = gl.createProgram();
+    this._update = function () {
 
-		var fragmentShader = gl.createShader( gl.FRAGMENT_SHADER );
+        var program = gl.createProgram();
 
-		gl.shaderSource( fragmentShader, this._fragmentShaderCode );
-		gl.compileShader( fragmentShader );
+        var fragmentShader = gl.createShader( gl.FRAGMENT_SHADER );
 
-		var vertexShader = gl.createShader( gl.VERTEX_SHADER );
+        gl.shaderSource( fragmentShader, this.fsCode );
+        gl.compileShader( fragmentShader );
 
-		gl.shaderSource( vertexShader, vertexShaderCode );
-		gl.compileShader(vertexShader);
+        var vertexShader = gl.createShader( gl.VERTEX_SHADER );
 
-		console.log( this._fragmentShaderCode )
+        gl.shaderSource( vertexShader, this.vsCode );
+        gl.compileShader( vertexShader );
 
-		if (!gl.getShaderParameter(fragmentShader, gl.COMPILE_STATUS)) {
-			var LOC = this._fragmentShaderCode.split('\n');
-			var dbgMsg = "ERROR: Could not build shader (fatal).\n\n------------------ KERNEL CODE DUMP ------------------\n"
+        console.log( this.fsCode )
 
-			// for (var nl = 0; nl < LOC.length; nl++)
-			// 	dbgMsg += (stdlib.split('\n').length + nl) + "> " + LOC[nl] + "\n";
+        if ( !gl.getShaderParameter( fragmentShader, gl.COMPILE_STATUS ) ) {
+            var LOC = this.fsCode.split( '\n' );
+            var dbgMsg = "ERROR: Could not build shader (fatal).\n\n------------------ KERNEL CODE DUMP ------------------\n"
 
-			dbgMsg += "\n--------------------- ERROR  LOG ---------------------\n" + gl.getShaderInfoLog(fragmentShader)
+            // for (var nl = 0; nl < LOC.length; nl++)
+            // 	dbgMsg += (stdlib.split('\n').length + nl) + "> " + LOC[nl] + "\n";
 
-			throw new Error(dbgMsg);
-		}
+            dbgMsg += "\n--------------------- ERROR  LOG ---------------------\n" + gl.getShaderInfoLog( fragmentShader )
 
-		gl.attachShader( program, vertexShader );
-		gl.attachShader( program, fragmentShader );
-		gl.linkProgram( program );
+            throw new Error( dbgMsg );
+        }
 
+        gl.attachShader( program, vertexShader );
+        gl.attachShader( program, fragmentShader );
+        gl.linkProgram( program );
 
-		// initShader( vertexShaderCode, this._fragmentShaderCode, program );
 
-		size = Math.sqrt( this.array.length ) / 4;
+        // initShader( this.vsCode, this.fsCode, program );
 
-		var uTexture = gl.getUniformLocation( program, 'u_texture' );
-		var aPosition = gl.getAttribLocation( program, 'a_position' );
-		var aUv = gl.getAttribLocation( program, 'a_uv' );
+        var uTexture = gl.getUniformLocation( program, 'u_texture' );
+        var aPosition = gl.getAttribLocation( program, 'a_position' );
+        var aUv = gl.getAttribLocation( program, 'a_uv' );
 
-		gl.useProgram(program);
+        gl.useProgram( program );
 
 
-		var dataTexture = createDataTexture( this.array );
+        var dataTexture = createDataTexture( this.array );
 
-		gl.viewport(0, 0, size, size);
-		gl.bindFramebuffer(gl.FRAMEBUFFER, gl.createFramebuffer());
+        gl.viewport( 0, 0, _size, _size );
+        gl.bindFramebuffer( gl.FRAMEBUFFER, gl.createFramebuffer() );
 
-		// Types arrays speed this up tremendously.
-		var resultTexture = createDataTexture( new Float32Array( this.array.length ) );
+        gl.framebufferTexture2D( gl.FRAMEBUFFER, gl.COLOR_ATTACHMENT0, gl.TEXTURE_2D, resultTexture, 0 );
 
-		gl.framebufferTexture2D(gl.FRAMEBUFFER, gl.COLOR_ATTACHMENT0, gl.TEXTURE_2D, resultTexture, 0);
+        gl.bindTexture( gl.TEXTURE_2D, dataTexture );
+        gl.activeTexture( gl.TEXTURE0 );
+        gl.uniform1i( uTexture, 0 );
+        gl.bindBuffer( gl.ARRAY_BUFFER, uvBuffer );
+        gl.enableVertexAttribArray( aUv );
+        gl.vertexAttribPointer( aUv, 2, gl.FLOAT, false, 0, 0 );
+        gl.bindBuffer( gl.ARRAY_BUFFER, positionBuffer );
+        gl.enableVertexAttribArray( aPosition );
+        gl.vertexAttribPointer( aPosition, 2, gl.FLOAT, false, 0, 0 );
+        gl.bindBuffer( gl.ELEMENT_ARRAY_BUFFER, indexBuffer );
+        gl.drawElements( gl.TRIANGLES, 6, gl.UNSIGNED_SHORT, 0 );
+        gl.readPixels( 0, 0, _size, _size, gl.RGBA, gl.FLOAT, this.array );
 
-		// Test for mobile bug MDN->WebGL_best_practices, bullet 7
-		var frameBufferStatus = (gl.checkFramebufferStatus(gl.FRAMEBUFFER) == gl.FRAMEBUFFER_COMPLETE);
+        // this.array = this.array.subarray(0, 4);
+        return this;
 
-		gl.bindTexture( gl.TEXTURE_2D, dataTexture );
-		gl.activeTexture ( gl.TEXTURE0 );
-		gl.uniform1i( uTexture, 0 );
-		gl.bindBuffer( gl.ARRAY_BUFFER, uvBuffer );
-		gl.enableVertexAttribArray( aUv );
-		gl.vertexAttribPointer( aUv, 2, gl.FLOAT, false, 0, 0 );
-		gl.bindBuffer( gl.ARRAY_BUFFER, positionBuffer );
-		gl.enableVertexAttribArray( aPosition );
-		gl.vertexAttribPointer( aPosition, 2, gl.FLOAT, false, 0, 0 );
-		gl.bindBuffer( gl.ELEMENT_ARRAY_BUFFER, indexBuffer );
-		gl.drawElements( gl.TRIANGLES, 6, gl.UNSIGNED_SHORT, 0 );
-		gl.readPixels( 0, 0, size, size, gl.RGBA, gl.FLOAT, this.array );
+    }
 
-		// this.array = this.array.subarray(0, 4);
-		return this;
+    // function initShader( this.vsCode, fragmentShaderCode, program ) {
 
-	}
+    //     console.log( this.vsCode, fragmentShaderCode )
 
-	function initShader( vertexShaderCode, fragmentShaderCode, program ) {
 
-		console.log( vertexShaderCode, fragmentShaderCode )
 
+    //     var fragmentShader = gl.createShader( gl.FRAGMENT_SHADER );
 
+    //     gl.shaderSource( fragmentShader, fragmentShaderCode );
+    //     gl.compileShader( fragmentShader );
 
-		var fragmentShader = gl.createShader( gl.FRAGMENT_SHADER );
 
-		gl.shaderSource( fragmentShader, fragmentShaderCode );
-		gl.compileShader( fragmentShader );
+    //     var vertexShader = gl.createShader( gl.VERTEX_SHADER );
 
+    //     gl.shaderSource( vertexShader, this.vsCode );
+    //     gl.compileShader( vertexShader );
 
-		var vertexShader = gl.createShader( gl.VERTEX_SHADER );
+    //     gl.attachShader( program, vertexShader );
+    //     gl.attachShader( program, fragmentShader );
+    //     gl.linkProgram( program );
 
-		gl.shaderSource( vertexShader, vertexShaderCode );
-		gl.compileShader(vertexShader);
+    //     if ( !gl.getProgramParameter( program, gl.LINK_STATUS ) ) {
+    //         var info = gl.getProgramInfoLog( program );
+    //         throw 'Could not compile WebGL program. \n\n' + info;
+    //     }
 
-		gl.attachShader( program, vertexShader );
-		gl.attachShader( program, fragmentShader );
-		gl.linkProgram( program );
 
-		if (!gl.getProgramParameter(program, gl.LINK_STATUS)) {
-			var info = gl.getProgramInfoLog(program);
-  			throw 'Could not compile WebGL program. \n\n' + info;
-		}
+    //     console.log( 'link' )
 
 
-		console.log( 'link' )
+    // }
 
+    function createDataTexture( data ) {
 
-	}
+        var texture = gl.createTexture();
 
-	function createDataTexture( data ) {
+        gl.bindTexture( gl.TEXTURE_2D, texture );
+        gl.texParameteri( gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.CLAMP_TO_EDGE );
+        gl.texParameteri( gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.CLAMP_TO_EDGE );
+        gl.texParameteri( gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.NEAREST );
+        gl.texParameteri( gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.NEAREST );
+        gl.texImage2D( gl.TEXTURE_2D, 0, gl.RGBA, _size, _size, 0, gl.RGBA, gl.FLOAT, data );
+        gl.bindTexture( gl.TEXTURE_2D, null );
 
-		var size = Math.sqrt( data.length ) / 4;
+        return texture;
+    }
 
-		var texture = gl.createTexture();
+    function getGLContext( canvas ) {
 
-		gl.bindTexture( gl.TEXTURE_2D, texture );
-		gl.texParameteri( gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.CLAMP_TO_EDGE );
-		gl.texParameteri( gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.CLAMP_TO_EDGE );
-		gl.texParameteri( gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.NEAREST );
-		gl.texParameteri( gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.NEAREST );
-		gl.texImage2D( gl.TEXTURE_2D, 0, gl.RGBA, size, size, 0, gl.RGBA, gl.FLOAT, data );
-		gl.bindTexture( gl.TEXTURE_2D, null );
+        var gl = null;
+        var attr = { alpha: false, antialias: false };
 
-		return texture;
-	}
+        var names = [ 'webgl', 'experimental-webgl', 'webkit-3d', 'moz-webgl' ];
 
-	function getGLContext( canvas ) {
+        for ( var i = 0; i < names.length; i++ ) {
 
-		var gl = null;
-		var attr = { alpha: false, antialias: false };
+            gl = canvas.getContext( names[ i ], attr );
 
-		var names = [ 'webgl', 'experimental-webgl', 'webkit-3d', 'moz-webgl' ];
+            if ( gl ) break;
+        }
 
-		for ( var i = 0; i < names.length; i ++ ) {
+        if ( !gl || !gl.getExtension( 'OES_texture_float' ) ) throw new Error( 'unsupported' );
 
-			gl = canvas.getContext( names[ i ], attr );
+        return gl;
+    }
 
-			if ( gl ) break;
-		}
+    this.commit = function () {
 
-		if ( !gl || !gl.getExtension( 'OES_texture_float' ) ) {
-			// error
-		}
+        this.fsCode += fragmentShaderCodeTail;
+        // initShader( this.vsCode, this.fsCode );
+        console.log( this.fsCode )
+        this._update();
 
-		return gl;
-	}
+        this.fsCode = fragmentShaderCodeHead;
 
-	this.commit = function() {
+        return this;
 
-		this._fragmentShaderCode += fragmentShaderCodeTail;
-		// initShader( vertexShaderCode, this._fragmentShaderCode );
-		this.refreshData();
+    }
 
-		this._fragmentShaderCode = fragmentShaderCodeHead;
+    this.set = function ( array, index ) {
 
-		return this.array;
+        index = index || 0;
+        this.array.set( array, index );
 
-	}
+        // TODO use gl.subTexImage2D
+        dataTexture = createDataTexture( this.array );
 
-	this.set = function( array, index ) {
-
-		index = index || 0;
-		this.array.set( array, index );
-
-		// TODO use gl.subTexImage2D
-		dataTexture = createDataTexture( this.array );
-
-		return this;
-	}
+        return this;
+    }
 
 
 }
 
 Para.prototype = {
-	constructor: Para,
 
-	_formatNumber: function( n ) {
+    constructor: Para,
 
-		if ( n % 1 === 0 ) return n + '.';
-		return n + '';
+    _formatNumber: function ( n ) {
 
-	},
+        if ( n % 1 === 0 ) return n + '.';
+        return n + '';
 
-	add: function ( n ) {
-		
-		var code = 'value += ' + this._formatNumber( n ) + ';\n';
+    },
 
-		this._fragmentShaderCode += code;
+    add: function ( n ) {
 
-		return this;
+        var code = '    value += ' + this._formatNumber( n ) + ';\n';
 
-	},
+        this.fsCode += code;
 
-	sub: function ( n ) {
+        return this;
 
-		var code = 'value -= ' + this._formatNumber( n ) + ';\n';
+    },
 
-		this._fragmentShaderCode += code;
+    sub: function ( n ) {
 
-		return this
+        var code = '    value -= ' + this._formatNumber( n ) + ';\n';
 
-		
-	},
+        this.fsCode += code;
 
-	mul: function ( n ) {
+        return this
 
-		var code = 'value *= ' + this._formatNumber( n ) + ';\n';
+    },
 
-		this._fragmentShaderCode += code;
+    mul: function ( n ) {
 
-		return this
+        var code = '    value *= ' + this._formatNumber( n ) + ';\n';
 
-	},
+        this.fsCode += code;
 
-	div: function ( n ) {
+        return this
 
-		var code = 'value /= ' + this._formatNumber( n ) + ';\n';
+    },
 
-		this._fragmentShaderCode += code;
+    div: function ( n ) {
 
-		return this
+        var code = '    value /= ' + this._formatNumber( n ) + ';\n';
 
-	},
+        this.fsCode += code;
 
-	sqrt: function () {
+        return this
 
-		return this
+    },
 
-	},
+    sqrt: function () {
 
-	abs: function () {
+        return this
 
-		return this
+    },
 
-	},
+    abs: function () {
 
-	neg: function () {
+        return this
 
-		return this
+    },
 
-	}
+    neg: function () {
+
+        return this
+
+    }
 }
